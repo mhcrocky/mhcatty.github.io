74fc5b690c305cbb352177494550d1981d5f03bd
es9d <larastar721@gmail.com>
2020-10-30 11:50:40 +0300

    df

---
+++ precache-manifest.21e3c28f7cad6ccfc07e4e3c467b7017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c662ef76420628922ea1008a79bf9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc902f8274f58577f5e0",
+    "url": "/static/css/main.eb50c708.chunk.css"
+  },
+  {
+    "revision": "686d3483d179fb5a1492",
+    "url": "/static/js/2.f702e7a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f702e7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc902f8274f58577f5e0",
+    "url": "/static/js/main.b2e04dde.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
